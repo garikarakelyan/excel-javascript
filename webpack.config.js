718f7ccdeeee768ad75861414bee6e701142ab4b
Garik Arakelyan <garik.arakelyan92@gmail.com>
2020-10-11 12:33:16 +0400

    finished styles

--- webpack.config.js
+++ webpack.config.js
@@ -8,16 +8,20 @@ const isProd = process.env.NODE_ENV === 'production';
 const isDev = !isProd;
 const filename = (ext) => isDev ? `bundle.${ext}` : `bundle.[hash].${ext}`;
 const jsLoaders = () => {
-  const loaders = {
-    loader: 'babel-loader',
-    options: {
-      presets: ['@babel/preset-env'],
-    },
-  }
+  const loaders = [
+    {
+      loader: 'babel-loader',
+      options: {
+        presets: ['@babel/preset-env']
+      }
+    }
+  ]
+
   if (isDev) {
-    loaders.push('eslint-loader');
+    loaders.push('eslint-loader')
   }
-  return loaders;
+
+  return loaders
 }
 
 module.exports = {
